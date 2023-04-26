2d127aaf747da71db70e17355ebbbae1bdac8fd0
Your Name <jimenez.llarves@gmail.com>
2023-04-26 17:31:17 +0800

    Add project files.

---
+++ CongApp/Pages/Meeting.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CongApp.Pages
+{
+    public class MeetingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
